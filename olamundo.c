e15f449725c252ba7e67f5dca6a52755e47aeba0
fernandatrindade135 <fernanda.trindade12@gmail.com>
2025-02-12 22:47:13 +0000

    tentativas de codigo

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+int idade , matricula;
+float altura;
+char nome [50];
+
+printf("Digite sua idade\n");
+scanf("%d, &idade");
+
+printf("Digite sua altura:\n");
+scanf("%f, &altura")
+
+printf("digite seu nome")
+scanf("%s, &nome");
+
+printf("Digite sua matricula")
+scanf("%d, &matricula ");
+
+printf("Nome do aluno: %s - Matricula: %d , nome, matricula");
+printf("Idade: %d - Altura: %f, idade, altura");
+
+return 0;
+
+}
